2a74765b7d83df2996c180fb399c4053a3b15134
joynul24 <joynula919@gmail.com>
2026-03-28 18:54:29 +0600

    feat: add coffee form with SweetAlert2 success/error popup
    feat: implement responsive CoffeeCard layout with Tailwind
    
    fix: clear form after successful coffee submission
    
    feat: fetch and display coffee list using axios
    
    refactor: improve CoffeeCard component structure and styling

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,11 +1,54 @@
 import { Link } from "react-router-dom";
 import Navber from "./shared/Navber";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 export default function AddCoffee() {
 
-    const handleAddCoffee=  (e)=> {
+    const handleAddCoffee = async (e)=> {
       e.preventDefault()
-      console.log("add coffee")
+      const formData = new FormData(e.target);
+      const newCoffee = {
+        name : formData.get("name"),
+        quantity: formData.get("quantity"),
+        supplier: formData.get("supplier"),
+        taste: formData.get("taste"),
+        category: formData.get("category"),
+        details: formData.get("details"),
+        price: formData.get("price"),
+        photo: formData.get("photo"),
+      }
+
+     // Validation check
+     if (Object.values(newCoffee).some(field => !field)) {
+       Swal.fire({
+       title: "Error!",
+       text: "Please fill all fields before submitting.",
+       icon: "error",
+       confirmButtonText: "OK"
+      });
+      return;
+    }
+
+     try{
+       const res = await axios.post("http://localhost:3000/coffees", newCoffee);
+       e.target.reset();
+       if(res.data.insertedId){
+         Swal.fire({
+        title: "Success!",
+        text: "Coffee added successfully!",
+        icon: "success",
+        confirmButtonText: "Cool"
+      });
+       }
+     }catch (error) {
+      Swal.fire({
+      title: "Error!",
+      text: "Something went wrong while adding coffee.",
+      icon: "error",
+      confirmButtonText: "Try Again"
+    });
+     }
     }
 
   return (
@@ -18,7 +61,7 @@ export default function AddCoffee() {
     </button>
   </Link>
   <div className="bg-[#F4F3F0] py-10 md:py-16 px-6 md:px-16 lg:px-28 rounded-2xl mt-10">
-    <h2 className="text-center text-2xl md:text-4xl font-bold">Add New Coffee</h2>
+    <h2 className="text-center text-2xl md:text-4xl font-bold font-rancho">Add New Coffee</h2>
     <p className="text-center max-w-3xl mx-auto my-4 font-raleway text-sm md:text-base">
       It is a long established fact that a reader will be distracted by
       the readable content of a page when looking at its layout. The point
@@ -102,9 +145,20 @@ export default function AddCoffee() {
           />
         </fieldset>
       </div>
-      {/* Photo Url row */}
-      <div className="flex flex-col gap-6">
-        <fieldset className="w-full fieldset">
+      {/* Price and phot URL */}
+      <div className="flex flex-col md:flex-row justify-between gap-6">
+         <fieldset className="w-full md:w-1/2 fieldset">
+          <label className="fieldset-label text-lg md:text-xl font-medium">
+            Price
+          </label>
+          <input
+            name="price"
+            type="text"
+            className="input w-full"
+            placeholder="price"
+          />
+        </fieldset>
+        <fieldset className="w-full fieldset md:w-1/2">
           <label className="fieldset-label text-lg md:text-xl font-medium">
             Photo Url
           </label>
@@ -116,7 +170,7 @@ export default function AddCoffee() {
           />
         </fieldset>
       </div>
-      <button className="btn w-full hover:bg-[#b39369] bg-[#D2B48C] border-[#331A15] mt-5">
+      <button className="btn w-full hover:bg-[#b39369] bg-[#D2B48C] border-[#331A15] mt-5 font-rancho">
         Add Coffee
       </button>
     </form>
--- src/components/CoffeeCards.jsx
+++ src/components/CoffeeCards.jsx
@@ -1,8 +1,59 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { FaEye, FaPen } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 export default function CoffeeCards() {
+  const [coffes, setCoffes] = useState([]);
+  useEffect(()=> {
+    axios.get("http://localhost:3000/coffees")
+    .then(res => setCoffes(res.data))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div>
-      Coffee Cards
+    <div className="">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 px-2">
+        {
+          coffes.map(coffee =>
+            (
+          <div key={coffee._id} 
+          className="flex flex-col md:flex-row items-center justify-between gap-6 p-7 rounded-lg bg-[#F5F4F1]"
+>
+         {/* coffee image */}
+        <img 
+        src={coffee.photo} 
+       alt={coffee.name} 
+       className="w-32 h-32 object-cover rounded-md" 
+      />
+
+     {/* coffee content */}
+  <div className="flex flex-col space-y-3 text-center md:text-left">
+    <h3 className="text-xl font-raleway">
+      <span className="font-bold">Name:</span> 
+      <span className="text-gray-600"> {coffee.name}</span>
+    </h3>
+    <p className="text-xl font-raleway">
+      <span className="font-bold">Chef:</span> 
+      <span className="text-gray-600"> {coffee.supplier}</span>
+    </p>
+    <p className="text-xl font-raleway">
+      <span className="font-bold">Price:</span> 
+      <span className="text-gray-600"> {coffee.price}</span>
+    </p>
+  </div>
+
+  {/* coffee actions */}
+  <div className="flex md:flex-col space-x-3 md:space-x-0 md:space-y-3 mt-4 md:mt-0">
+    <button className="btn bg-amber-400"><FaEye /></button>
+    <button className="btn bg-gray-600 text-white border-0"><FaPen /></button>
+    <button className="btn bg-red-500 border-0 text-white"><MdDelete /></button>
+  </div>
+</div>
+    )
+   )
+ }
+  </div>
     </div>
   )
 }
